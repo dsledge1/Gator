72be97f9d56442d40eea0f11786bdc0db5b704b0
dsledge1 <david.sledge1@gmail.com>
2026-06-01 08:01:40 -0400

    initial

--- main.go
+++ main.go
@@ -15,3 +15,53 @@ func main() {
 	cfg.SetUser("david")
 	config.Read()
 }
+
+func login() {
+
+}
+
+func registerUser() {
+
+}
+
+func users() {
+
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("login expects a single argument: the username")
+	}
+	err := s.config.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println("User set to: ", s.config.User)
+	return nil
+}
+
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	login        command
+	registerUser command
+	users        command
+	handlerMap   map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	if handler, ok := commands.handlerMap[cmd]; ok {
+
+	}
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+
+}
